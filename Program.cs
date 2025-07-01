21cb8362f1c99e32f8ba068dd88d0f9d39c33a89
LaviniaHorberth <142851295+LaviniaHorberth@users.noreply.github.com>
2025-07-01 16:24:49 -0300

    Exercicio 1002 BeeCrowd

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            double pi = 3.14159;
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = (Math.Pow(raio, 2.0))*pi;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
